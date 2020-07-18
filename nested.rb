582038b179cba8f4be9104e2abb2e1dbcf2c002b
Samara Grant <samara.amanda01@gmail.com>
2020-07-18 23:59:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -38,6 +38,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -56,8 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     
-     programmer_hash[:alan_kay][:known_for]
+
 end
 
 def adding_matz
