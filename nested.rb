be7cf5f9d8857253a9c75a4c854f3da8a28f3248
Samara Grant <samara.amanda01@gmail.com>
2020-07-19 00:14:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -113,8 +113,6 @@ def changing_alan
      programmer_hash[:alan_kay][:known_for] = alans_new_info
 
      programmer_hash
-
-
 end
 
 def adding_to_dennis
@@ -133,5 +131,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 
 end
