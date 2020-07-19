d44046aaccc357ea334db230ad1905d9a14c4665
Samara Grant <samara.amanda01@gmail.com>
2020-07-19 00:12:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -88,8 +88,9 @@ def adding_matz
         }
      }
      programmer_hash
-
 end
+
+
 
 def changing_alan
 	programmer_hash =
@@ -109,6 +110,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
 
 
 end
